f0efabe143fc4c487c5e4f297f81d3c58729d605
Anton <laposhchenkoanton@gmail.com>
2025-05-06 10:15:45 +0300

    Remove the welcome endpoint

--- src/test/java/pharmacy/welcome/WelcomeControllerIntegrationTests.java
+++ src/test/java/pharmacy/welcome/WelcomeControllerIntegrationTests.java
@@ -15,11 +15,11 @@
  *
 package pharmacy.welcome;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+// import static org.hamcrest.CoreMatchers.*;
+// import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
