5d77ec9d56a1347047f87ec41036d624f899a711
Anton <laposhchenkoanton@gmail.com>
2025-05-09 15:11:56 +0300

    Add special states for lab orders

--- src/main/java/pharmacy/lab/LabOrder.java
+++ src/main/java/pharmacy/lab/LabOrder.java
@@ -5,9 +5,14 @@ import org.salespointframework.payment.PaymentMethod;
 import org.salespointframework.useraccount.UserAccount.UserAccountIdentifier;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 
 @Entity
 public class LabOrder extends Order {
+  @Enumerated(EnumType.STRING)
+  private LabOrderStatus labStatus = LabOrderStatus.OPEN;
+
   public LabOrder() {
     super();
   }
@@ -18,5 +23,32 @@ public class LabOrder extends Order {
 
   public LabOrder(UserAccountIdentifier id, PaymentMethod pm) {
     super(id, pm);
+  }
+
+  public boolean isOpenLab() {
+    return labStatus == LabOrderStatus.OPEN;
+  }
+
+  public boolean isInProgress() {
+    return labStatus == LabOrderStatus.IN_PROGRESS;
+  }
+
+  public boolean isReadyForPickup() {
+    return labStatus == LabOrderStatus.READY_FOR_PICKUP;
+  }
+
+  public boolean isPickedUp() {
+    return labStatus == LabOrderStatus.PICKED_UP;
+  }
+
+  public void setLabStatus(LabOrderStatus newStatus) {
+    labStatus = newStatus;
+  }
+
+  public enum LabOrderStatus {
+    OPEN,
+    IN_PROGRESS,
+    READY_FOR_PICKUP,
+    PICKED_UP,
   }
 }
